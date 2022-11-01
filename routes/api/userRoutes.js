539201fce9287e856f57b2ffca4ef51b00aed5de
jonacko <jessonacko@gmail.com>
2022-11-01 16:08:03 +0000

    completed insomnia testing

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,6 +3,10 @@ const {
   getUsers,
   getSingleUser,
   createUser,
+  updateUsers,
+  deleteUsers,
+  addFriend,
+  deleteFriend
 } = require('../../controllers/userController');
 
 // /api/users
@@ -10,5 +14,10 @@ router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
 router.route('/:userId').get(getSingleUser);
+
+// .put(updateUsers).delete(deleteUsers); ----- put this at end of line 16
+
+// // Directs to: /api/users/:userId/friends/:friendId <POST, DELETE>
+// router.route('/:id/friends/:friendId').post(addFriend).delete(deleteFriend)
 
 module.exports = router;
